9464eac252f7910ce9faa60c738345f3931bc82a
Yash Dhakad <yashvdkd@gmail.com>
2024-02-18 18:43:30 +0530

    style: login page updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,20 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import Image from "next/image";
+import bg from "@/app/assets/x.gif"
 
 const page = () => {
   return (
-    <main className="h-screen grid grid-cols-12">
-      <section className="col-span-7"></section>
-      <section className="px-20 flex items-center justify-center flex-col gap-y-4 border col-span-5">
+    <main className="h-screen grid sm:grid-cols-12">
+      <section className="col-span-7 flex items-center justify-center flex-col gap-y-4 relative">
+        <Image className="w-full h-full opacity-30 absolute -z-10" src={bg} alt="bg" />
+        <h1 className="text-7xl font-black text-yellow-400 italic tracking-tight text-center uppercase">Quantum One</h1>
+        {/* <p className="text-muted-foreground">
+          Building your business Quantums, One at a time!
+        </p> */}
+      </section>
+      <section className="px-20 flex items-center justify-center flex-col gap-y-4 col-span-5">
         <div className="text-center">
           <h3 className="text-2xl font-semibold tracking-tight">
             Create an account
@@ -15,12 +23,20 @@ const page = () => {
             Enter your email below to create your account.
           </p>
         </div>
-        <Input type="email" placeholder="name@example.com" />
-        <Input type="password" placeholder="password" />
+        <div className="w-full flex gap-x-2">
+          <Input type="email" placeholder="name@example.com" />
+          <Input type="password" placeholder="password" />
+        </div>
         <Button className="w-full">Sign In with Email</Button>
-        <p className="text-sm text-muted-foreground">Or Continue With</p>
-        <Button variant="outline" className="w-full"><FaGoogle /> &nbsp; Google</Button>
-        <Button variant="outline" className="w-full"><FaGithub /> &nbsp; Github</Button>
+        <p className="text-sm text-muted-foreground">or continue with</p>
+        <div className="w-full flex gap-x-2">
+          <Button variant="outline" className="w-full">
+            <FaGoogle /> &nbsp; Google
+          </Button>
+          <Button variant="outline" className="w-full">
+            <FaGithub /> &nbsp; Github
+          </Button>
+        </div>
       </section>
     </main>
   );
