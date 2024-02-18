f10c6618cbdfabdb31368c368317ce332a6a38cf
Yash Dhakad <yashvdkd@gmail.com>
2024-02-18 22:46:35 +0530

    feat: dark mode added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,20 +7,20 @@ import bg from "@/app/assets/x.gif";
 const page = () => {
   return (
     <main className="h-screen grid sm:grid-cols-12">
-      <section className="col-span-7 flex items-center justify-center flex-col gap-y-4 relative">
+      <section className="col-span-6 flex items-center justify-center flex-col gap-y-4 relative">
         <Image
-          className="w-full h-full opacity-30 absolute -z-10"
+          className="w-full h-full absolute -z-10"
           src={bg}
           alt="bg"
         />
-        <h1 className="text-7xl font-black text-yellow-400 italic tracking-tight text-center uppercase">
-          Quantum One
+        <h1 className="text-7xl text-white font-black italic tracking-tight text-center uppercase">
+          Q1
         </h1>
         {/* <p className="text-muted-foreground">
           Building your business Quantums, One at a time!
         </p> */}
       </section>
-      <section className="px-20 flex items-center justify-center flex-col gap-y-4 col-span-5">
+      <section className="px-20 flex items-center justify-center flex-col gap-y-4 col-span-6 bg-gradient-to-br from-slate-950 to-slate-900">
         <div className="text-center">
           <h3 className="text-2xl font-semibold tracking-tight">
             Create an account
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -8,19 +10,49 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
-
-import { Switch } from "@/components/ui/switch";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Link from "next/link";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function Page() {
+  const { setTheme } = useTheme();
   return (
     <main className="h-screen flex flex-col justify-between items-center">
-      <section className="">
-        Dark Mode <Switch />
+      <section>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <FaMoon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <FaSun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Link href="/login">Login</Link>
       </section>
-      <section>Hero</section>
-      <section>Footer</section>
+      <section>
+        
+        <h1>Quantum One</h1>
+      </section>
+      <section>&copy; Quantum One 2024</section>
     </main>
   );
 }
