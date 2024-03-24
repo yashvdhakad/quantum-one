a56ede7bb85354c550ac402fb205dc31bbf2041e
Yash Dhakad <yashvdkd@gmail.com>
2024-03-25 00:31:35 +0530

    refactor: :fire: minor changes

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -5,11 +5,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const LoginPage = () => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState<{ email: string } | null>(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = async (email, password) => {
+  const login = async (email: string, password: string) => {
     const session = await account.createEmailPasswordSession(email, password);
     setLoggedInUser(await account.get());
   };
@@ -36,9 +36,9 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <p>Please Login or Register to proceed further!</p>
-      <forms className="flex flex-col justify-center items-center gap-4">
+    <section className="h-screen flex flex-col justify-center items-center gap-6">
+      <h1 className="text-center">Hello,<br />This an authentication<br />testing page for Q1</h1>
+      <div className="flex flex-col justify-center items-center gap-4">
         <Input
           type="email"
           placeholder="name@example.com"
@@ -53,16 +53,16 @@ const LoginPage = () => {
         />
 
         <div className="flex gap-4 items-center">
-          <Button variant={"outline"} onClick={() => login(email, password)}>
+          <Button onClick={() => login(email, password)}>
             Login
           </Button>
           <span>or</span>
-          <Button variant={"outline"} onClick={register}>
+          <Button onClick={register}>
             Register
           </Button>
         </div>
-      </forms>
-    </div>
+      </div>
+    </section>
   );
 };
 
